f09e81e266a483e6d11bfa0487ea633eb93d9eac
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2013-05-08 18:42:23 +0200

    Prevent infinite loops
    Conflicts:
    
    	public/onerror.js

--- public/onerror.js
+++ public/onerror.js
@@ -1,10 +1,22 @@
-window.onerror = function(errorMsg, file, lineNumber) {
-    try {
-        var url = window.location.href;
-        var logUrl = '/log?msg=' + encodeURIComponent(errorMsg) + '&url=' + encodeURIComponent(url);
+(function() {
 
-        new Image().src = logUrl;
-    } catch(e) {
-        // we don't want errors to throw inside onerror
-    }
-};
+    var maxNumberOfErrors = 5;
+    var errorCount = 0;
+
+    window.onerror = function(errorMsg, file, lineNumber) {
+        // prevent infinite loops
+        errorCount += 1;
+        if (errorCount > maxNumberOfErrors) return;
+
+        try {
+            var url = window.location.href;
+            var logUrl = '/log?msg=' + encodeURIComponent(errorMsg) + '&url=' + encodeURIComponent(url);
+
+            new Image().src = logUrl;
+        } catch(e) {
+            // we don't want errors to throw inside onerror
+        }
+    };
+
+})();
+
